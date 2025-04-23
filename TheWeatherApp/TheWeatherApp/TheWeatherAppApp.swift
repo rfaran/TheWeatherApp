b29b15307b0d32be9632aa18cfaba7eda60eff94
faran <faran_rasheed@hotmail.com>
2025-04-23 15:01:49 -0400

    project init

---
+++ TheWeatherApp/TheWeatherApp/TheWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheWeatherAppApp.swift
+//  TheWeatherApp
+//
+//  Created by Faran Rasheed on 4/23/25.
+//
+
+import SwiftUI
+
+@main
+struct TheWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
